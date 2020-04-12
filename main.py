90a7560d1e678753620f86fc1ee7730abcb4e16a
YEN <cglogc53112@gmail.com>
2020-04-13 00:26:11 +0800

    fixmain

--- main.py
+++ main.py
@@ -30,6 +30,32 @@ FROM post_shared_train
 """
 shared_train = pd.read_sql(query, engine)
 
+query = """
+SELECT *
+FROM post_comment_created_train
+"""
+comment_train = pd.read_sql(query, engine)
+
+query = """
+SELECT *
+FROM post_liked_train
+"""
+like_train = pd.read_sql(query, engine)
+
+#收藏
+query = """
+SELECT *
+FROM post_collected_train
+"""
+collected_train = pd.read_sql(query, engine)
+
+items = {
+    "shared" : shared_train,
+    "comment" : comment_train,
+    "liked" : like_train,
+    "collected" : collected_train
+}
+
 def add_other_c(hot,oth):
     key = hot['post_key']
     hour = hot['created_at_hour']
@@ -38,14 +64,14 @@ def add_other_c(hot,oth):
     
     ac_count = 0
     
-    
     if len(conform) >= 1:
         ac_count = conform[conform['created_at_hour']< (hour + datetime.timedelta(hours=36))]['count'].sum()
     return ac_count
 
-# hot_train['share'] = hot_train.apply(lambda x : add_other_c(x, shared_train),axis=1)
+for i in items:
+    s = time.time()
+    hot_train[i] = hot_train.apply(lambda x : add_other_c(x,items[i]),axis=1)
+    e = time.time()
+    print("{} cost times ==>{}".format(i,e-s))
 
-s = time.time()
-hot_train['share'] = hot_train.apply(lambda x : add_other_c(x,shared_train),axis=1)
-e = time.time()
-print(e-s)
+hot_train.to_csv('main_train.csv')
